acf43cd13c1dcaca1279caf8c95447355a8a6d96
KaMZeSs <vladd267@gmail.com>
2021-11-27 22:30:08 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Generate_all_repetitions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Repetition repetition = new Repetition(new char[] { 'a', 'b', 'c', 'd' });
+            
+            for (String str = String.Empty; str.Length <= 4; str = new String(repetition.nextStep()))
+            {
+                Console.WriteLine(str);
+
+            }
+        }
+    }
+}
